bfed9af84eb5f5fd649694de623eea0e4dbfdfe9
Davfool888 <davfool888@gmail.com>
2025-09-25 19:51:32 -0500

    Componen context

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,14 +1,17 @@
-export function TaskCard({task, deleteTask}) {
 
 
+
+function TaskCard({task, deleteTask}) {
   return (
-    <div>
+
+      <div>
       <h1>{task.title}</h1>
       <p>{task.description}</p>
       <button onClick={() => deleteTask(task.id)}>
         Eliminar tarea
       </button>
     </div>
+
   );
 }
 
---
+++ src/components/context/TaskContext.jsx
@@ -0,0 +1,10 @@
+function TaskContext(props) {
+  return (
+    <>
+    <h1>Componente context</h1>
+      {props.children}
+    </>
+  )
+}
+
+export default TaskContext
