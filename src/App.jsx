b04f0a04f28eb423314046d364fb1fc4765a579d
Davfool888 <davfool888@gmail.com>
2025-09-27 19:22:06 -0500

    Estilizando app con tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ function App() {
   
 
   return (
-    <>
-      <TaskForm/>
+    <main className="bg-zinc-900 h-screen">
+      <div  className="container mx-auto p-10">
+        <TaskForm/>
       <TaskList/>
+      </div>
 
-    </>
+    </main>
   );
 }
 
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -19,20 +19,25 @@ function TaskForm() {
   };
 
   return (
-    <form onSubmit={handleSumit}>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSumit} className="bg-slate-800 p-10 mb-4">
+      <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
       <input
         placeholder="Escribe tu Tarea "
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className="bg-slate-300 p-3 w-full mb-2"
         autoFocus
       />
       <textarea
         placeholder="Escribe descripcion de la tarea"
         onChange={(e) => setDescription(e.target.value)}
         value={description}
+        className="bg-slate-300 p-3 w-full mb-2"
       ></textarea>
-      <button>Guardar</button>
+      <button className="text-white p-4 bg-blue-500 rounded-md">Guardar</button>
     </form>
+    </div>
   );
 }
 
